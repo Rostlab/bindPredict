df9c2d673b767de31a92cc577c5785ab944c9b8c
mariasche <maria.schelling@tum.de>
2017-04-17 16:06:57 +0200

    Prof parser written

---
+++ parsers/ProfParser.py
@@ -0,0 +1,4 @@
+"""Provides methods to parser output 
+from secondary structure and solvent accessibility predictions like ProfPHD and Reprof"""
+
+def _parse_file(file_in):
---
+++ parsers/RunParser.py
@@ -0,0 +1,15 @@
+from ProfParser import ProfParser
+
+file = 'Q9Y530.reprof'
+file2 = 'Q9Y530.prof'
+pp2 = ProfParser(file2, method='profphd')
+pp = ProfParser(file, method='reprof')
+
+print(pp.seq)
+print(pp.p_rel_solv)
+
+print(pp2.seq)
+print(pp2.p_rel_solv)
+print(pp2.o_rel_solv_10)
+
+print(pp.o_rel_solv_10)
