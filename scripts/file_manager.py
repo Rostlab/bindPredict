af00c6d65c5a28ac3dd5a145d55f968b683fefd6
mariasche <mariasche@github.com>
2018-05-21 00:09:17 +0200

    Added documentation

--- scripts/file_manager.py
+++ scripts/file_manager.py
@@ -1,9 +1,12 @@
+""" Class specifying file endings and locations.
+Before usage, values should be adjusted to local settings"""
+
+
 class FileManager(object):
 
     @property
     def ec_suffix(self):
         return ".di_scores"
-
 
     @property
     def dist_suffix(self):
@@ -13,7 +16,6 @@ class FileManager(object):
     def evmut_suffix(self):
         return ".epistatic_effect"
 
-
     @property
     def cons_suffix(self):
         return "_frequencies.csv"
@@ -22,22 +24,19 @@ class FileManager(object):
     def align_suffix(self):
         return "_alignment_statistics.csv"
 
-
     @property
     def ids_file(self):
         file_name = "ids_split"
         return file_name
 
-
     @property
     def ids_folder(self):
         return "data/id_lists/"
-
 
     @property
     def binding_file(self):
         return "data/binding_residues_evc.txt"
 
-
+    @property
     def blosum_file(self):
         return "data/blosum_62.txt"
