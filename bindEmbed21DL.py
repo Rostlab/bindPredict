67e880d46585ae581f6dcd70c5128d8c28a4b7cb
Maria Littmann <littmann@rostlab.org>
2021-09-06 14:19:13 +0200

    Scripts for development of bindEmbed21DL added

---
+++ bindEmbed21DL.py
@@ -0,0 +1,138 @@
+from data_preparation import ProteinInformation
+from ml_trainer import MLTrainer
+from ml_predictor import MLPredictor
+from config import FileSetter, FileManager
+
+import numpy as np
+from sklearn.model_selection import PredefinedSplit
+
+
+class BindEmbed21DL(object):
+
+    @staticmethod
+    def cross_train_pipeline(params, model_output, predictions_output, ri):
+        """
+        Run cross-training pipeline for a specific set of parameters
+        :param params:
+        :param model_output: If None, trained model is not written
+        :param predictions_output: If None, predictions are not written
+        :param ri: Should RI or raw probabilities be written?
+        :return:
+        """
+
+        print("Prepare data")
+        ids = []
+        fold_array = []
+        for s in range(1, 6):
+            ids_in = '{}{}.txt'.format(FileSetter.split_ids_in(), s)
+            split_ids = FileManager.read_ids(ids_in)
+
+            ids += split_ids
+            fold_array += [s] * len(split_ids)
+
+        ps = PredefinedSplit(fold_array)
+
+        # get sequences + maximum length + labels
+        sequences, max_length, labels = ProteinInformation.get_data(ids)
+
+        proteins = dict()
+        trainer = MLTrainer(pos_weights=params['weights'])
+
+        for train_index, test_index in ps.split():
+
+            split_counter = fold_array[test_index[0]]
+            train_ids, validation_ids = ids[train_index], ids[test_index]
+
+            print("Train model")
+            model_split = trainer.train_validate(params, train_ids, validation_ids, sequences, labels, max_length,
+                                                 verbose=False)
+
+            if model_output is not None:
+                model_path = '{}{}.pt'.format(model_output, split_counter)
+                FileManager.save_classifier_torch(model_split, model_path)
+
+            print("Calculate predictions per protein")
+            ml_predictor = MLPredictor(model_split)
+            curr_proteins = ml_predictor.predict_per_protein(validation_ids, sequences, labels, max_length)
+
+            proteins = {**proteins, **curr_proteins}
+
+            if predictions_output is not None:
+                FileManager.write_predictions(proteins, predictions_output, 0.5, ri)
+
+        return proteins
+
+    @staticmethod
+    def hyperparameter_optimization_pipeline(params, num_splits, result_file):
+        """
+        Development pipeline used to optimize hyperparameters
+        :param params:
+        :param num_splits:
+        :param result_file:
+        :return:
+        """
+
+        print("Prepare data")
+        ids = []
+        fold_array = []
+        for s in range(1, num_splits + 1):
+            ids_in = '{}{}.txt'.format(FileSetter.split_ids_in(), s)
+            split_ids = FileManager.read_ids(ids_in)
+
+            ids += split_ids
+            fold_array += [s] * len(split_ids)
+
+        ids = np.array(ids)
+
+        # get sequences + maximum length + labels
+        sequences, max_length, labels = ProteinInformation.get_data(ids)
+
+        print("Perform hyperparameter optimization")
+        trainer = MLTrainer(pos_weights=params['weights'])
+        del params['weights']  # remove weights to not consider as parameter for optimization
+
+        model = trainer.cross_validate(params, ids, fold_array, sequences, labels, max_length, result_file)
+
+        return model
+
+    @staticmethod
+    def prediction_pipeline(model_prefix, cutoff, result_folder, ids_in, fasta_file, ri):
+        """
+        Run predictions with bindEmbed21DL for a given list of proteins
+        :param model_prefix:
+        :param cutoff: Cutoff to use to define prediction as binding (default: 0.5)
+        :param result_folder:
+        :param ids_in:
+        :param fasta_file:
+        :param ri: Should RI or raw probabilities be written?
+        :return:
+        """
+
+        print("Prepare data")
+        ids = FileManager.read_ids(ids_in)
+        sequences, max_length, labels = ProteinInformation.get_data_predictions(ids, fasta_file)
+
+        proteins = dict()
+        for i in range(0, 5):
+            print("Load model")
+            model_path = '{}{}.pt'.format(model_prefix, i + 1)
+            model = FileManager.load_classifier_torch(model_path)
+
+            print("Calculate predictions")
+            ml_predictor = MLPredictor(model)
+            curr_proteins = ml_predictor.predict_per_protein(ids, sequences, labels, max_length)
+
+            for k in curr_proteins.keys():
+                if k in proteins.keys():
+                    prot = proteins[k]
+                    prot.add_predictions(curr_proteins[k].predictions)
+                else:
+                    proteins[k] = curr_proteins[k]
+
+        for k in proteins.keys():
+            proteins[k].normalize_predictions(5)
+
+        if result_folder is not None:
+            FileManager.write_predictions(proteins, result_folder, cutoff, ri)
+
+        return proteins
---
+++ ml_trainer.py
@@ -0,0 +1,338 @@
+from sklearn.model_selection import PredefinedSplit
+from collections import defaultdict
+import itertools as it
+
+import torch
+
+from architectures import CNN2Layers
+from data_preparation import MyDataset
+from assess_performance import PerformanceAssessment as PerformAssess, ModelPerformance
+from config import FileManager, GeneralInformation
+from pytorchtools import EarlyStopping
+
+
+class MLTrainer(object):
+
+    def __init__(self, pos_weights, batch_size=203):
+        self.batch_size = batch_size
+        if torch.cuda.is_available():
+            self.device = 'cuda:0'
+        else:
+            self.device = 'cpu'
+
+        self.pos_weights = torch.tensor(pos_weights).to(self.device)
+
+    def train_validate(self, params, train_ids, validation_ids, sequences, labels, max_length, verbose=True):
+        """
+        Train & validate predictor for one set of parameters and ids
+        :param params:
+        :param train_ids:
+        :param validation_ids:
+        :param sequences:
+        :param labels:
+        :param max_length:
+        :param verbose:
+        :return:
+        """
+
+        model, train_performance, val_performance = self._train_validate(params, train_ids, validation_ids, sequences,
+                                                                         labels, max_length, verbose=verbose)
+
+        train_loss, train_acc, train_prec, train_recall, train_f1, train_mcc = \
+            train_performance.get_performance_last_epoch()
+        val_loss, val_acc, val_prec, val_recall, val_f1, val_mcc = val_performance.get_performance_last_epoch()
+
+        print("Train loss: {:.3f}, Prec: {:.3f}, Recall: {:.3f}, F1: {:.3f}, MCC: {:.3f}".format(train_loss,
+                                                                                                 train_prec,
+                                                                                                 train_recall,
+                                                                                                 train_f1,
+                                                                                                 train_mcc))
+        print("Val loss: {:.3f}, Prec: {:.3f}, Recall: {:.3f}, F1: {:.3f}, MCC: {:.3f}".format(val_loss,
+                                                                                               val_prec,
+                                                                                               val_recall, val_f1,
+                                                                                               val_mcc))
+
+        return model
+
+    def cross_validate(self, params, ids, fold_array, sequences, labels, max_length, result_file):
+        """
+        Perform cross-validation to optimize hyperparameters
+        :param params:
+        :param ids:
+        :param fold_array:
+        :param sequences:
+        :param labels:
+        :param max_length:
+        :param result_file:
+        :return:
+        """
+        ps = PredefinedSplit(fold_array)
+
+        # create parameter grid
+        param_sets = defaultdict(dict)
+        sorted_keys = sorted(params.keys())
+        param_combos = it.product(*(params[s] for s in sorted_keys))
+        counter = 0
+        for p in list(param_combos):
+            curr_params = list(p)
+            param_dict = dict(zip(sorted_keys, curr_params))
+            param_sets[counter] = param_dict
+
+            counter += 1
+
+        best_score = 0
+        best_params = dict()  # save best parameter set
+        best_classifier = None  # save best classifier
+        performance = defaultdict(list)  # save performance for each parameter combination
+
+        params_counter = 1
+
+        for p in param_sets.keys():
+            curr_params = param_sets[p]
+            print('{}\t{}'.format(params_counter, curr_params))
+
+            model = None
+
+            train_model_performance = ModelPerformance()
+            val_model_performance = ModelPerformance()
+
+            for train_index, test_index in ps.split():
+                train_ids, validation_ids = ids[train_index], ids[test_index]
+
+                model, train_performance, val_performance = self._train_validate(curr_params, train_ids, validation_ids,
+                                                                                 sequences, labels, max_length)
+
+                train_loss, train_acc, train_prec, train_recall, train_f1, train_mcc = \
+                    train_performance.get_performance_last_epoch()
+                val_loss, val_acc, val_prec, val_recall, val_f1, val_mcc = val_performance.get_performance_last_epoch()
+
+                train_model_performance.add_single_performance(train_loss, train_acc, train_prec, train_recall,
+                                                               train_f1, train_mcc)
+
+                val_model_performance.add_single_performance(val_loss, val_acc, val_prec, val_recall, val_f1, val_mcc)
+
+            # take average over all splits
+            train_loss, train_acc, train_prec, train_recall, train_f1, train_mcc = \
+                train_model_performance.get_mean_performance()
+            val_loss, val_acc, val_prec, val_recall, val_f1, val_mcc = val_model_performance.get_mean_performance()
+
+            performance['train_precision'].append(train_prec)
+            performance['train_recall'].append(train_recall)
+            performance['train_f1'].append(train_f1)
+            performance['train_mcc'].append(train_mcc)
+            performance['train_acc'].append(train_acc)
+            performance['train_loss'].append(train_loss)
+
+            performance['val_precision'].append(val_prec)
+            performance['val_recall'].append(val_recall)
+            performance['val_f1'].append(val_f1)
+            performance['val_mcc'].append(val_mcc)
+            performance['val_acc'].append(val_acc)
+            performance['val_loss'].append(val_loss)
+
+            for param in curr_params.keys():
+                performance[param].append(curr_params[param])
+
+            if val_f1 > best_score:
+                best_score = val_f1
+                best_params = curr_params
+                best_classifier = model
+
+            params_counter += 1
+
+        FileManager.save_cv_results(performance, result_file)
+
+        print(best_score)
+        print(best_params)
+
+        return best_classifier
+
+    def _train_validate(self, params, train_ids, validation_ids, sequences, labels, max_length, verbose=True):
+        """
+        Train and validate bindEmbed21DL model
+        :param params:
+        :param train_ids:
+        :param validation_ids:
+        :param sequences:
+        :param labels:
+        :param max_length:
+        :param verbose:
+        :return:
+        """
+
+        # define data sets
+        train_set = MyDataset(train_ids, sequences, labels, max_length)
+        validation_set = MyDataset(validation_ids, sequences, labels, max_length)
+
+        train_loader = torch.utils.data.DataLoader(train_set, batch_size=self.batch_size, shuffle=True, pin_memory=True,
+                                                   worker_init_fn=GeneralInformation.seed_worker)
+        validation_loader = torch.utils.data.DataLoader(validation_set, batch_size=self.batch_size, shuffle=True,
+                                                        pin_memory=True, worker_init_fn=GeneralInformation.seed_worker)
+
+        pos_weights = self.pos_weights.expand(max_length, 3)
+        pos_weights = pos_weights.t()
+
+        loss_fun = torch.nn.BCEWithLogitsLoss(reduction='none', pos_weight=pos_weights)
+        sigm = torch.nn.Sigmoid()
+
+        padding = int((params['kernel'] - 1) / 2)
+        model = CNN2Layers(train_set.get_input_dimensions(), params['features'], params['kernel'], params['stride'],
+                           padding, params['dropout'])
+        model.to(self.device)
+
+        optim_args = {'lr': params['lr'], 'betas': params['betas'], 'eps': params['eps'],
+                      'weight_decay': params['weight_decay']}
+        optimizer = torch.optim.Adamax(model.parameters(), **optim_args)
+
+        checkpoint_file = 'checkpoint_early_stopping.pt'
+        early_stopping = EarlyStopping(patience=10, delta=0.01, checkpoint_file=checkpoint_file)
+
+        train_performance = PerformAssess()
+        validation_performance = PerformAssess()
+
+        num_epochs = 0
+
+        for epoch in range(params['epochs']):
+            if verbose:
+                print("Epoch {}".format(epoch))
+
+            train_loss = val_loss = 0
+            train_loss_count = val_loss_count = 0
+            train_tp = train_tn = train_fn = train_fp = 0
+            val_tp = val_tn = val_fn = val_fp = 0
+
+            train_acc = train_prec = train_rec = train_f1 = train_mcc = 0
+            val_acc = val_prec = val_rec = val_f1 = val_mcc = 0
+
+            # training
+            model.train()
+            for in_feature, target, loss_mask in train_loader:
+                optimizer.zero_grad()
+                in_feature = in_feature.to(self.device)
+                in_feature_1024 = in_feature[:, :-1, :]
+
+                target = target.to(self.device)
+                loss_mask = loss_mask.to(self.device)
+                pred = model.forward(in_feature_1024)
+
+                # don't consider padded positions for loss calculation
+                loss_el = loss_fun(pred, target)
+                loss_el_masked = loss_el * loss_mask
+
+                loss_norm = torch.sum(loss_el_masked)
+
+                train_loss += loss_norm.item()
+                train_loss_count += in_feature.shape[0]
+
+                for idx, i in enumerate(in_feature):  # remove padded positions to calculate tp, fp, tn, fn
+                    pred_i, target_i = GeneralInformation.remove_padded_positions(pred[idx], target[idx], i)
+
+                    pred_i = sigm(pred_i)
+                    tp_i, fp_i, tn_i, fn_i = PerformAssess.evaluate_per_residue_torch(pred_i.detach().cpu(),
+                                                                                      target_i.detach().cpu(), 3)
+                    train_tp += tp_i
+                    train_fp += fp_i
+                    train_tn += tn_i
+                    train_fn += fn_i
+
+                    acc, prec, rec, f1, mcc = PerformAssess.calc_performance_measurements(tp_i, fp_i, tn_i, fn_i)
+                    train_acc += acc
+                    train_prec += prec
+                    train_rec += rec
+                    train_f1 += f1
+                    train_mcc += mcc
+
+                loss_norm.backward()
+                optimizer.step()
+
+            # validation
+            model.eval()
+            with torch.no_grad():
+                for in_feature, target, loss_mask in validation_loader:
+                    in_feature = in_feature.to(self.device)
+                    in_feature_1024 = in_feature[:, :-1, :]
+
+                    target = target.to(self.device)
+                    loss_mask = loss_mask.to(self.device)
+
+                    pred = model.forward(in_feature_1024)
+
+                    # don't consider padded position for loss calculation
+                    loss_el = loss_fun(pred, target)
+                    loss_el_masked = loss_el * loss_mask
+                    val_loss += torch.sum(loss_el_masked).item()
+                    val_loss_count += in_feature.shape[0]
+
+                    for idx, i in enumerate(in_feature):  # remove padded positions to calculate tp, fp, tn, fn
+                        pred_i, target_i = GeneralInformation.remove_padded_positions(pred[idx], target[idx], i)
+
+                        pred_i = sigm(pred_i)
+                        tp_i, fp_i, tn_i, fn_i = PerformAssess.evaluate_per_residue_torch(pred_i.detach().cpu(),
+                                                                                          target_i.detach().cpu(), 3)
+                        val_tp += tp_i
+                        val_fp += fp_i
+                        val_tn += tn_i
+                        val_fn += fn_i
+
+                        acc, prec, rec, f1, mcc = PerformAssess.calc_performance_measurements(tp_i, fp_i, tn_i, fn_i)
+
+                        val_acc += acc
+                        val_prec += prec
+                        val_rec += rec
+                        val_f1 += f1
+                        val_mcc += mcc
+
+            train_loss = train_loss / (train_loss_count * 3)
+            val_loss = val_loss / (val_loss_count * 3)
+
+            train_acc = train_acc / train_loss_count
+            train_prec = train_prec / train_loss_count
+            train_rec = train_rec / train_loss_count
+            train_f1 = train_f1 / train_loss_count
+            train_mcc = train_mcc / train_loss_count
+
+            val_acc = val_acc / val_loss_count
+            val_prec = val_prec / val_loss_count
+            val_rec = val_rec / val_loss_count
+            val_f1 = val_f1 / val_loss_count
+            val_mcc = val_mcc / val_loss_count
+
+            if verbose:
+                print("Train loss: {:.3f}, Prec: {:.3f}, Recall: {:.3f}, F1: {:.3f}, MCC: {:.3f}".format(train_loss,
+                                                                                                         train_prec,
+                                                                                                         train_rec,
+                                                                                                         train_f1,
+                                                                                                         train_mcc))
+                print('TP: {}, FP: {}, TN: {}, FN: {}'.format(train_tp, train_fp, train_tn, train_fn))
+                print("Val loss: {:.3f}, Prec: {:.3f}, Recall: {:.3f}, F1: {:.3f}, MCC: {:.3f}".format(val_loss,
+                                                                                                       val_prec,
+                                                                                                       val_rec, val_f1,
+                                                                                                       val_mcc))
+                print('TP: {}, FP: {}, TN: {}, FN: {}'.format(val_tp, val_fp, val_tn, val_fn))
+            train_performance.loss_epochs.append(train_loss)
+            train_performance.acc_epochs.append(train_acc)
+            train_performance.mcc_epochs.append(train_mcc)
+            train_performance.f1_epochs.append(train_f1)
+            train_performance.prec_epochs.append(train_prec)
+            train_performance.recall_epochs.append(train_rec)
+
+            validation_performance.loss_epochs.append(val_loss)
+            validation_performance.acc_epochs.append(val_acc)
+            validation_performance.mcc_epochs.append(val_mcc)
+            validation_performance.f1_epochs.append(val_f1)
+            validation_performance.prec_epochs.append(val_prec)
+            validation_performance.recall_epochs.append(val_rec)
+
+            num_epochs += 1
+
+            # stop training if F1 score doesn't improve anymore
+            if 'early_stopping' in params.keys() and params['early_stopping']:
+                eval_val = val_f1 * (-1)
+                # eval_val = val_loss
+                early_stopping(eval_val, model, verbose)
+                if early_stopping.early_stop:
+                    break
+
+        model = torch.load(checkpoint_file)
+
+        return model, train_performance, validation_performance
