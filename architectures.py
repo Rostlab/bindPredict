66e461a29d0db426ddf4147ea0096fae4490904d
Maria Littmann <littmann@rostlab.org>
2021-09-06 12:37:07 +0200

    1st version of bindEmbed21DL added

---
+++ architectures.py
@@ -0,0 +1,20 @@
+import torch.nn
+
+
+class CNN2Layers(torch.nn.Module):
+
+    def __init__(self, in_channels, feature_channels, out_channels, kernel_size, stride, padding, dropout):
+        super(CNN2Layers, self).__init__()
+        self.conv1 = torch.nn.Sequential(
+            torch.nn.Conv1d(in_channels=in_channels, out_channels=feature_channels, kernel_size=kernel_size,
+                            stride=stride, padding=padding),
+            torch.nn.ELU(),
+            torch.nn.Dropout(dropout),
+
+            torch.nn.Conv1d(in_channels=feature_channels, out_channels=out_channels, kernel_size=kernel_size,
+                            stride=stride, padding=padding),
+        )
+
+    def forward(self, x):
+        x = self.conv1(x)
+        return torch.squeeze(x)
