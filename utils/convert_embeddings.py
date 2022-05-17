becad35758f0a6c06e4035920a480188b7ff770d
Maria Littmann <maria.littmann@tum.de>
2022-05-17 13:28:05 +0200

    Script for embedding conversion added

--- utils/convert_embeddings.py
+++ utils/convert_embeddings.py
@@ -1,23 +1,23 @@
+"""
+Script to convert embeddings generated with the bio_embeddings pipeline in the expected format
+"""
+
 import h5py
 import numpy as np
+import sys
 
 
 def main():
+    embeddings_in = sys.argv[1]
+    embeddings_out = sys.argv[2]
 
-    embeddings_in = ''  # TODO give path to .h5-file of embeddings generated with bio_embeddings pipeline
-    embeddings_out = ''  # TODO set output directory
+    with h5py.File(embeddings_in, 'r') as f_in:
+        with h5py.File(embeddings_out, 'w') as f_out:
+            for key,embedding in f_in.items():
+                original_id = embedding.attrs['original_id']
+                embedding = np.array(embedding)
 
-    embeddings = dict()
-    with h5py.File(embeddings_in, 'r') as f:
-        for key, embedding in f.items():
-            original_id = embedding.attrs['original_id']
-            embeddings[original_id] = np.array(embedding)
-
-    for k in embeddings.keys():
-        embedding = embeddings[k]
-
-        out_file = '{}{}.npy'.format(embeddings_out, k)
-        np.save(out_file, embedding)
+                f_out.create_dataset(original_id, data=embedding)
 
 
 main()
